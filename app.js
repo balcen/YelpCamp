5d361e4b7d2eed5d59bd5a77bd901239bcc0eab0
baiceng <jameslu8044@gmail.com>
2019-04-13 15:21:46 +0800

    fix port

--- app.js
+++ app.js
@@ -46,6 +46,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, function () {
+app.listen(process.env.PORT, process.env.IP, function () {
     console.log("listening on port 3000");
 });
