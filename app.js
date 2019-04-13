d1844584c20f5ed6d864170c58635bba6f853b93
baiceng <jameslu8044@gmail.com>
2019-04-13 15:16:30 +0800

    css login/register

--- app.js
+++ app.js
@@ -46,6 +46,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function () {
+app.listen(3000, function () {
     console.log("listening on port 3000");
 });
