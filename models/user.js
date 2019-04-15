31c5b2e021279c07331fee08568f0fd03cf20b63
baiceng <jameslu8044@gmail.com>
2019-04-15 18:31:17 +0800

    fix undefine username img

--- models/user.js
+++ models/user.js
@@ -6,7 +6,10 @@ var userSchema = new mongoose.Schema({
     password: "String",
     firstName: "String",
     lastName: "String",
-    avatar: "String",
+    avatar: {
+        type:"String",
+        default: "https://images.unsplash.com/photo-1552343204-09c57040d686?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80"
+    },
     email: "String"
 });
 
